38fda2475fd4177c1305953ac7f2e3d081763ee5
oskar-paulsson <oskar@paulsson.dev>
2021-04-10 13:28:14 +0200

    Create myFirstSmartContract.sol
    a very simple contract

---
+++ myFirstSmartContract.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.7.0;
+
+contract FirstContract{
+
+  // state variable; count
+  uint count;
+
+  constructor(){
+    count = 10;
+  }
+
+  // a getter function for that variable
+  function getCount() public view returns(uint){
+    return count;
+  }
+
+  //
+  function addNumber(uint number) public {
+    count = count + number;
+  }
+
+  function subtractNumber(uint number) public {
+    count = count - number;
+  }
+}
