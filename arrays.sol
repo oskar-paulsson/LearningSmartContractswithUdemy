7e8d6b86bca424688d7e89ddd50d542aa25538cd
oskar-paulsson <oskar@paulsson.dev>
2021-04-11 21:44:52 +0200

    Update arrays.sol

---
+++ arrays.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.0;
+
+contract ArraysExample{
+
+    uint[5] fixedSizeArray = [1, 2, 3, 4, 5];
+    uint[] public dynamicArray;
+
+    // add elements
+    constructor() {
+        dynamicArray.push(5);
+        dynamicArray.push(6);
+        dynamicArray.push(9);
+    }
+
+    // update a specific element
+    function updateArray(uint _index, uint _updatedValue) externally {
+        dynamicArray[_index] = _updatedValue;
+    }
+
+    // push a specific element
+    function pushElement(uint _updatedValue) externally {
+        dynamicArray.push(_updatedValue);
+    }
+
+    // delete an elements
+    function deleteArray(uint _index) external {
+        delete dynamicArray[_index];
+    }
+
+    // add a number to each element in the array
+    function addNumbers(uint _number) external {
+        for(uint i = 0; i < numbers.length; i++){
+            numbers[i] = numbers[i] + _number;
+        }
+    }
+}
