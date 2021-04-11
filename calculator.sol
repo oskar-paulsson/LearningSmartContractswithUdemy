d87abc17424b8c4a9576f4953da1defcaf3d063d
oskar-paulsson <64486383+oskar-paulsson@users.noreply.github.com>
2021-04-11 13:11:50 +0200

    Update calculator.sol
    A very simple calculator

--- calculator.sol
+++ calculator.sol
@@ -1,28 +1,50 @@
 pragma solidity ^0.7.0;
 
+// A simple calculator, written in solidity
+
 contract Calculator{
 
-  // state variable;
-  uint firstNumber;
-  uint secondNumber;
-  uint result;
+    // state variable;
+    uint firstNumber;
+    uint secondNumber;
+    uint result;
 
-  constructor(){
-    count = 10;
-  }
+    constructor() {
+        firstNumber = 0;
+        secondNumber = 0;
+        result = 0;
+    }
 
-  function calculate() public view returns(uint){
+    function setFirstFactor(uint number) public {
+        firstNumber = number;
+    }
 
+    function setSecondFactor(uint number) public {
+        secondNumber = number;
+    }
+
+    function multiply() public {
+        result = firstNumber * secondNumber;
+        getCount();
+    }
+
+    function divide() public {
+        result = firstNumber / secondNumber;
+        getCount();
+    }
+
+    function subtract() public {
+        result = firstNumber - secondNumber;
+        getCount();
+    }
     
-    return result;
-  }
-
-  //
-  function addNumber(uint number) public {
-    count = count + number;
-  }
-
-  function subtractNumber(uint number) public {
-    count = count - number;
-  }
+    function add() public {
+        result = firstNumber + secondNumber;
+        getCount();
+    }
+    
+    function getCount() public view returns(uint){
+        return result;
+    }
+    
 }
