b87387846d5310a8b76de519e7e6936db65d4b2d
oskar-paulsson <oskar@paulsson.dev>
2021-04-11 18:45:09 +0200

    Create mappings.sol
    simple examples of mappings

---
+++ mappings.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.0;
+
+contract MappingExample{
+
+    // initiate simple MappingExample
+    mapping(string => uint) public players;
+
+    // fill up the mapping with data
+    constructor (){
+        players['bob'] = 20;
+        players['alice'] = 50;
+        players['carl'] = 10;
+    }
+
+    // reading data from a mapping
+    function(string memory _player) external view returns(){
+        return players[_player];
+    }
+
+    // update
+    function updateScore(string memory _player, uint _score) external {
+        players[_player] = _score;
+    }
+
+    // delete a players
+    function deletePlayer(string memory _player) external {
+        delete players[_player];
+    }
+}
