9e49d09427bac02dbb193420866c06873a264935
oskar-paulsson <oskar@paulsson.dev>
2021-05-22 15:51:17 +0200

    Update types.sol

--- types.sol
+++ types.sol
@@ -1,2 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.7.0;
+
+contract ValueTypes {
+
+    uint[] public x = [1,2,3,4,5]
+
+    function realTest() external {
+        _changeSomething(x);
+    }
+
+    function _changeSomething(uint[] storage _input) internal return(uint[] memory) {
+        _input[0] = 42;
+        return _input;
+    }
+
+}
