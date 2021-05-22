5f66f2d52bb853b157a184df3d0f91e97c4af32b
oskar-paulsson <oskar@paulsson.dev>
2021-05-22 16:18:32 +0200

    Create functions.sol

---
+++ functions.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.0;
+
+// functions(<paramater types>)
+//     {internal | external | public | private} visibility modifiers
+//     [pure | view | payable] restriction modifiers
+//     [(custom modifiers)]
+//     [returns (<return types>)]
